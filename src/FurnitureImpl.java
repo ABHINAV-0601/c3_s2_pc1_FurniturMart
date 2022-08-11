c4e2e70dbd42c354e1887e2a443acefdd130ad79
Abhinav Pandey <pandeyabhinav2608@gmail.com>
2022-08-11 13:01:13 +0530

    completed all the printing statements

--- src/FurnitureImpl.java
+++ src/FurnitureImpl.java
@@ -16,6 +16,26 @@ public class FurnitureImpl {
         furnitureItem.color = sc.nextLine();
         System.out.println("Enter where is the usage of furniture (indoor/outdoor)");
         furnitureItem.furnitureUsage = sc.nextLine();
+        System.out.println("Enter the price of the furniture :");
+        furnitureItem.price = sc.nextDouble();
+
+        System.out.println("Code of the furniture is :" + furnitureItem.furnitureCode);
+        System.out.println("Type of the furniture is :" + furnitureItem.furnitureType);
+        System.out.println("Grade of the furniture is :" + furnitureItem.gradeOfFurniture);
+        System.out.println("Color of the furniture is :" + furnitureItem.color);
+        System.out.println("Furniture is to use :" + furnitureItem.furnitureUsage);
+
+        if(furnitureItem.furnitureUsage == "indoor"){
+            System.out.println("Price of the furniture is :" + furnitureItem.price);
+        }
+
+        if(furnitureItem.furnitureUsage == "outdoor"){
+            System.out.println("Price of the furniture is :" + furnitureItem.discount(5));
+        }
+
+
+
+
 
     }
 }
